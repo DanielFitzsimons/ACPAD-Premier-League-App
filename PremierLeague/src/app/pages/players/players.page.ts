37c0037dffb6aaf2e6167e4775355b669f5e3388
Daniel Fitzsimons <80958065+DanielFitzsimons@users.noreply.github.com>
2024-01-12 14:18:35 +0000

    adding team players based on id

--- PremierLeague/src/app/pages/players/players.page.ts
+++ PremierLeague/src/app/pages/players/players.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FootballService } from 'src/app/services/football.service';
-
+import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-players',
   templateUrl: './players.page.html',
@@ -8,20 +8,23 @@ import { FootballService } from 'src/app/services/football.service';
 })
 export class PlayersPage implements OnInit {
   teamIds = [33, 34, 35, 36, 39, 40, 42, 44, 45, 47, 48, 49, 50, 51, 52, 55, 62, 65, 66];
-  selectedTeamId: number | null = null; // Initially no team is selected
+  selectedTeamId: number | null = null; 
   players: any[] = [];
+  teamInfo: any;
 
-  constructor(private footballService: FootballService) {}
+  constructor(private footballService: FootballService, private navCtrl: NavController) {}
 
   ngOnInit() {
-    // Optionally preload players for a default team or leave it until user selection
+    this.selectedTeamId = this.teamIds[0];
+    this.loadTeamPlayers();
   }
 
   loadTeamPlayers() {
     if (this.selectedTeamId) {
       this.footballService.getTeamPlayers(this.selectedTeamId).subscribe(
-        players => {
-          this.players = players; // Assign the players data to the array
+        data => {
+          this.players = data.players; // Assign the players data to the array
+          this.teamInfo = data.team; // Assign the team data to the teamInfo property
         },
         error => {
           console.error('There was an error retrieving the team players:', error);
@@ -29,4 +32,9 @@ export class PlayersPage implements OnInit {
       );
     }
   }
+
+  goHome(){
+    this.navCtrl.navigateForward('/home');
+  }
+  
 }
