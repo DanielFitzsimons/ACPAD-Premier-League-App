c32ba2d73a449be0c355e40e7bd14f1b3a01b691
Daniel Fitzsimons <80958065+DanielFitzsimons@users.noreply.github.com>
2024-01-14 11:09:40 +0000

    Added Components for popover

--- PremierLeague/src/app/app.module.ts
+++ PremierLeague/src/app/app.module.ts
@@ -12,9 +12,10 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { HttpClientModule } from '@angular/common/http';
 import { PremierLeagueComponent } from './premier-league/premier-league.component';
 import { FootballService } from './services/football.service';
-
+import { AssistsPopoverComponent } from './info/assists-popover/assists-popover.component';
+import { TeamsSquadsPopoverComponent } from './info/teams-squads-popover/teams-squads-popover.component';
 @NgModule({
-  declarations: [AppComponent, PremierLeagueComponent,],
+  declarations: [AppComponent, PremierLeagueComponent, AssistsPopoverComponent, TeamsSquadsPopoverComponent],
   imports: [BrowserModule, IonicModule.forRoot(),  AppRoutingModule, FormsModule, CommonModule, provideFirebaseApp(() => initializeApp({
     apiKey: "AIzaSyChHLlEaPrZAEZIf-zYppHzxYucBxU8UMY",
     authDomain: "premierleagueapp-581df.firebaseapp.com",
