114e4d61b5d8ed07d685677cf647186032b39bf7
Daniel Fitzsimons <80958065+DanielFitzsimons@users.noreply.github.com>
2024-01-14 11:05:52 +0000

    Added popover and css to home page

--- PremierLeague/src/app/home/home.page.ts
+++ PremierLeague/src/app/home/home.page.ts
@@ -1,42 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { AuthenticationService } from '../services/authentication.service';
 import { FootballService } from '../services/football.service';
+import { PickerController, PopoverController } from '@ionic/angular';
+import { AssistsPopoverComponent } from '../info/assists-popover/assists-popover.component';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  topScorers: any[] = [];
-  seasons: string[] = ['2010','2019','2020', '2021', '2022'];
+  playerStats: any[] = [];
+  seasons: string[] = ['2023', '2022', '2021', '2020', '2019', '2018', '2017', '2016', '2015','2014','2013', '2012', '2011', '2010'];
   selectedSeason: string = this.seasons[0];
-  constructor(private navCtrl: NavController, private auth: AuthenticationService, private footballService: FootballService) {}
+  statType: 'scorers' | 'assists' = 'scorers';
+
+  topScorersCount: number | undefined;
+
+  constructor(private navCtrl: NavController, private auth: AuthenticationService, private footballService: FootballService, private pickerCtrl: PickerController, private popoverController: PopoverController ) {}
 
   // In your component
 ngOnInit() {
-  this.loadTopScorers();
+  this.loadPlayerStats();
 }
 
-loadTopScorers() {
-  const leagueId = '39'; // The league ID for which you want top scorers
-  this.footballService.getTopScorers(leagueId, this.selectedSeason).subscribe(
-    topScorersData => {
-      this.topScorers = topScorersData.map((scorer: any) => {
-        return {
-          name: scorer.player.name,
-          goals: scorer.statistics[0].goals.total,
-          team: scorer.statistics[0].team.name,
-          photo: scorer.player.photo,
-          
-        };
-      });
-    },
-    error => {
-      console.error('Error fetching top scorers:', error);
+ loadPlayerStats() {
+    const leagueId = '39';
+    if (this.statType === 'scorers') {
+      this.footballService.getTopScorers(leagueId, this.selectedSeason).subscribe(
+        data => this.processPlayerStats(data),
+        error => console.error('Error fetching player stats:', error)
+      );
+    } else {
+      this.footballService.getTopAssists(leagueId, this.selectedSeason).subscribe(
+        data => this.processPlayerStats(data),
+        error => console.error('Error fetching player stats:', error)
+      );
     }
-  );
-}
+  }
+
+  processPlayerStats(data: any) {
+    this.playerStats = data.map((item: any) => {
+      return {
+        name: item.player.name,
+        count: this.statType === 'scorers' ? item.statistics[0].goals.total : item.statistics[0].goals.assists,
+        team: item.statistics[0].team.name,
+        photo: item.player.photo,
+      };
+    });
+    this.topScorersCount = this.playerStats.length;
+  }
+
+  showScorers() {
+    this.statType = 'scorers';
+    this.loadPlayerStats();
+  }
+
+  showAssists() {
+    this.statType = 'assists';
+    this.loadPlayerStats();
+  }
+
+
 
 
 
@@ -53,6 +78,39 @@ loadTopScorers() {
     this.navCtrl.navigateForward('/players')
   }
 
+  async openSeasonPicker() {
+    const picker = await this.pickerCtrl.create({
+      columns: [
+        {
+          name: 'season',
+          options: this.seasons.map(season => ({ text: season, value: season }))
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Confirm',
+          handler: (value) => {
+            this.selectedSeason = value.season.value;
+            this.loadPlayerStats();
+          }
+        }
+      ]
+    });
+    await picker.present();
+  }
+
+  async presentPopover(ev: any, popoverContent: TemplateRef<any>) {
+    const popover = await this.popoverController.create({
+      component: AssistsPopoverComponent,
+      event: ev,
+      translucent: true
+    });
+    return await popover.present();
+  }
 
   async logout() {
     await this.auth.logout();
